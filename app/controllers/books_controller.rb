96a33a241406fe10f3143a208653ce3819044ff5
EC2 Default User <ec2-user@ip-172-31-1-162.ap-northeast-1.compute.internal>
2023-08-28 13:14:08 +0000

    [Add]subject3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,25 @@
 class BooksController < ApplicationController
+  
+  def create
+    
 
   def index
-    
+    @books = books.all
   end
 
   def show
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :destroy]
   resources :users, only: [:index, :show]
   get 'homes/about' => 'homes#about', as: 'about'
 end
